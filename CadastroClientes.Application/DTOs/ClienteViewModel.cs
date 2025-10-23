5da0a468ca66844166ecfc49149b37202b3ae71e
Brayan Sullyvan <125376880+BrayanSMS@users.noreply.github.com>
2025-10-23 20:06:53 -0300

    Implementação Camada de Aplicação (CadastroClientes)

---
+++ CadastroClientes.Application/DTOs/ClienteViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace CadastroClientes.Application.DTOs
+{
+    public class ClienteViewModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public static ClienteViewModel FromEntity(Domain.Entities.Cliente cliente)
+        {
+            return new ClienteViewModel
+            {
+                Id = cliente.Id,
+                Nome = cliente.Nome,
+                Email = cliente.Email,
+                Status = cliente.Status.ToString(),
+                DataCadastro = cliente.DataCadastro
+            };
+        }
+    }
+}
